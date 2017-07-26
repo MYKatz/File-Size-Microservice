72dbf107e6d11c336b01390e133ebc16fc27446a
System <none>
2017-07-26 00:33:01 +0000

    🍼🎬 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,30 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var multer = require('multer');
+var upload = multer();
+var app = express();
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
+app.post("/upload", upload.single('upl'), function (request, response) {
+  response.json({"size": request.file["size"] });
+});
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
